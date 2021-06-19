68929c55b9569316cbfd526fd832eff211da9cc3
ArchivalLegion <55867818+ArchivalLegion@users.noreply.github.com>
2021-06-19 09:21:47 -0400

    Update plzno-part2.sh

--- plzno-part2.sh
+++ plzno-part2.sh
@@ -6,38 +6,19 @@ apt update &&
 dpkg-reconfigure locales tzdata keyboard-configuration console-setup &&
 
 
-apt install --yes nano &&
+apt install --yes nano dosfstools cryptsetup curl patch ubuntu-standard grub-efi-amd64 grub-efi-amd64-signed linux-image-generic shim-signed zfs-initramfs &&
 
 
-apt install --yes dosfstools &&
-
-
-
-mkdosfs -F 32 -s 1 -n "$EFILABEL" "$DISK"-part1 &&
+mkdosfs -F 32 -n "$EFILABEL" "$DISK"-part1 &&
 mkdir /boot/efi &&
 echo /dev/disk/by-uuid/$(blkid -s UUID -o value $DISK-part1) \
 /boot/efi vfat defaults 0 0 >> /etc/fstab &&
 mount /boot/efi &&
 
 
-
 mkdir /boot/efi/grub /boot/grub &&
 echo /boot/efi/grub /boot/grub none defaults,bind 0 0 >> /etc/fstab &&
 mount /boot/grub &&
-
-
-
-apt install --yes \
-grub-efi-amd64 grub-efi-amd64-signed linux-image-generic \
-shim-signed zfs-initramfs &&
-
-
-
-apt remove --purge --yes os-prober &&
-
-
-apt install --yes cryptsetup &&
-
 
 
 echo swap $DISK-part2 /dev/urandom \
@@ -48,9 +29,6 @@ echo /dev/mapper/swap none swap defaults 0 0 >> /etc/fstab &&
 addgroup --system lpadmin &&
 addgroup --system lxd &&
 addgroup --system sambashare &&
-
-
-sudo apt install --yes curl patch &&
 
 
 curl https://launchpadlibrarian.net/478315221/2150-fix-systemd-dependency-loops.patch | \
@@ -64,6 +42,10 @@ update-grub &&
 
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi \
+--bootloader-id=$BOOTID --recheck --compress=no &&
+
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi \
 --bootloader-id=$BOOTID --recheck --compress=no --removable &&
 
 
@@ -71,8 +53,9 @@ mkdir /etc/zfs/zfs-list.cache &&
 touch /etc/zfs/zfs-list.cache/bpool &&
 touch /etc/zfs/zfs-list.cache/rpool &&
 ln -s /usr/lib/zfs-linux/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d &&
-echo "WAIT 10 seconds and kill zed"
-zed -Fvf &&
+echo "WAIT 10 seconds and kill zed" &&
+echo "Trying to automate zed" &&
+sleep 1;zed -Fvf;sleep 10;killall zed &&
 
 
 sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/bpool &&
@@ -91,12 +74,6 @@ chown -R $USER:$USER /home/$USER &&
 usermod -a -G adm,cdrom,dip,lpadmin,lxd,plugdev,sambashare,sudo $USER &&
 
 
-apt dist-upgrade --yes &&
-
-
-apt install --yes ubuntu-standard &&
-
-
 for file in /etc/logrotate.d/* ; do
     if grep -Eq "(^|[^#y])compress" "$file" ; then
         sed -i -r "s/(^|[^#y])(compress)/\1#\2/" "$file"
@@ -106,6 +83,4 @@ done
 zfs snapshot bpool/BOOT/"$RDATASET"_"$UUID"@install &&
 zfs snapshot rpool/ROOT/"$RDATASET"_"$UUID"@install &&
 
-echo "If you've gotten this far, stop being lazy, set root password, do zed -F &"
-
-echo "firefox https://openzfs.github.io/openzfs-docs/Getting%20Started/Ubuntu/Ubuntu%2020.04%20Root%20on%20ZFS.html#step-5-grub-installation &"
+echo "If you've gotten this far, set root password, do zed -Fvf and kill it"
