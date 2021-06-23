07858ac1234e3ef9064866f67610ed2424cd0036
ArchivalLegion <55867818+ArchivalLegion@users.noreply.github.com>
2021-06-23 04:49:42 -0400

    Update plzno-part2.sh

--- plzno-part2.sh
+++ plzno-part2.sh
@@ -1,64 +1,63 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-apt update &&
+echo "Update package index, setup system, and install needed packages" && {
+apt update
+dpkg-reconfigure locales tzdata keyboard-configuration console-setup
+apt install --yes nano dosfstools cryptsetup curl patch ubuntu-standard grub-efi-amd64 grub-efi-amd64-signed linux-image-generic shim-signed zfs-initramfs
+}
 
-dpkg-reconfigure locales tzdata keyboard-configuration console-setup &&
+echo "Format EFI partition and bind /boot/efi/grub" && {
+mkdosfs -F 32 -n "$EFILABEL" "$DISK"-part1
+mkdir /boot/efi
+echo /dev/disk/by-uuid/$(blkid -s UUID -o value $DISK-part1) /boot/efi vfat defaults 0 0 >> /etc/fstab
+mount /boot/efi
+mkdir /boot/efi/grub /boot/grub
+echo /boot/efi/grub /boot/grub none defaults,bind 0 0 >> /etc/fstab
+mount /boot/grub
+}
 
-apt install --yes nano dosfstools cryptsetup curl patch ubuntu-standard grub-efi-amd64 grub-efi-amd64-signed linux-image-generic shim-signed zfs-initramfs &&
+echo "Encrypt swap partition" && {
+echo swap $DISK-part2 /dev/urandom swap,cipher=aes-xts-plain64:sha256,size=512 >> /etc/crypttab
+echo /dev/mapper/swap none swap defaults 0 0 >> /etc/fstab
+}
 
+echo "Adding system groups" && {
+addgroup --system lpadmin
+addgroup --system lxd
+addgroup --system sambashare
+addgroup --system gpio
+addgroup --system i2c
+addgroup --system input
+addgroup --system spi
+}
 
-mkdosfs -F 32 -n "$EFILABEL" "$DISK"-part1 &&
-mkdir /boot/efi &&
-echo /dev/disk/by-uuid/$(blkid -s UUID -o value $DISK-part1) \
-/boot/efi vfat defaults 0 0 >> /etc/fstab &&
-mount /boot/efi &&
-
-mkdir /boot/efi/grub /boot/grub &&
-echo /boot/efi/grub /boot/grub none defaults,bind 0 0 >> /etc/fstab &&
-mount /boot/grub &&
-
-
-echo swap $DISK-part2 /dev/urandom \
-swap,cipher=aes-xts-plain64:sha256,size=512 >> /etc/crypttab &&
-echo /dev/mapper/swap none swap defaults 0 0 >> /etc/fstab &&
-
-
-addgroup --system lpadmin &&
-addgroup --system lxd &&
-addgroup --system sambashare &&
-addgroup --system gpio &&
-addgroup --system i2c &&
-addgroup --system input &&
-addgroup --system spi &&
-
+echo "Patch a dependency loop" && {
 curl https://launchpadlibrarian.net/478315221/2150-fix-systemd-dependency-loops.patch | \
-sed "s|/etc|/lib|;s|\.in$||" | (cd / ; sudo patch -p1) &&
+sed "s|/etc|/lib|;s|\.in$||" | (cd / ; sudo patch -p1)
+}
 
-
-update-initramfs -c -k all &&
-
-
-update-grub &&
-
-
+echo "Updating kernel images and install GRUB" && {
+update-initramfs -c -k all
+update-grub
 grub-install --target=x86_64-efi --efi-directory=/boot/efi \
---bootloader-id=$BOOTID --recheck --compress=no &&
-
-
+--bootloader-id=$BOOTID --recheck --compress=no
 grub-install --target=x86_64-efi --efi-directory=/boot/efi \
---bootloader-id=$BOOTID --recheck --compress=no --removable &&
+--bootloader-id=$BOOTID --recheck --compress=no --removable
+}
 
-
-mkdir /etc/zfs/zfs-list.cache &&
-touch /etc/zfs/zfs-list.cache/bpool &&
-touch /etc/zfs/zfs-list.cache/rpool &&
-ln -s /usr/lib/zfs-linux/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d &&
-echo "Trying to automate zed" &&
-sleep 1;zed -Fvf;sleep 10;killall zed &&
-echo "zed finished" &&
-sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/bpool &&
-sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/rpool &&
-echo "sed ran" &&
+echo "Fix filesystem mount ordering" && {
+mkdir /etc/zfs/zfs-list.cache
+touch /etc/zfs/zfs-list.cache/bpool
+touch /etc/zfs/zfs-list.cache/rpool
+ln -s /usr/lib/zfs-linux/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d
+echo "Running zed"
+timeout -s 15 -k 10 zed -F
+echo "zed killed"
+sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/bpool
+sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/rpool
+echo "sed ran"
+}
 
 ROOT_DS=$(zfs list -o name | awk '/ROOT\/"$RDATASET"_/{print $1;exit}') &&
 zfs create -o com.ubuntu.zsys:bootfs-datasets=$ROOT_DS \
@@ -77,7 +76,9 @@ for file in /etc/logrotate.d/* ; do
     fi
 done
 
-zfs snapshot bpool/BOOT/"$RDATASET"_"$UUID"@install &&
-zfs snapshot rpool/ROOT/"$RDATASET"_"$UUID"@install &&
+echo "Create inital system snapshot" && {
+zfs snapshot bpool/BOOT/"$RDATASET"_"$UUID"@bpool_install
+zfs snapshot rpool/ROOT/"$RDATASET"_"$UUID"@rpool_install
+}
 
-echo "If you've gotten this far, set root password, do zed -Fvf and kill it"
+echo "Finished! Enjoy the system"
