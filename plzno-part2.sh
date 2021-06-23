b26003ddd3eda9529c34e66f288f2a8c69e5e9a4
ArchivalLegion <55867818+ArchivalLegion@users.noreply.github.com>
2021-06-23 05:59:15 -0400

    Update plzno-part2.sh

--- plzno-part2.sh
+++ plzno-part2.sh
@@ -10,19 +10,17 @@ apt install --yes nano dosfstools cryptsetup curl patch ubuntu-standard grub-efi
 echo "Format EFI partition and bind /boot/efi/grub" && {
 mkdosfs -F 32 -n "$EFILABEL" "$DISK"-part1
 mkdir /boot/efi
-}
-echo /dev/disk/by-uuid/$(blkid -s UUID -o value $DISK-part1) /boot/efi vfat defaults 0 0 >> /etc/fstab && {
+echo /dev/disk/by-uuid/$(blkid -s UUID -o value $DISK-part1) /boot/efi vfat defaults 0 0 >> /etc/fstab
 mount /boot/efi
 mkdir /boot/efi/grub /boot/grub
-}
-echo /boot/efi/grub /boot/grub none defaults,bind 0 0 >> /etc/fstab && {
+echo /boot/efi/grub /boot/grub none defaults,bind 0 0 >> /etc/fstab
 mount /boot/grub
 }
 
-echo "Encrypt swap partition"
+echo "Encrypt swap partition" && {
 echo swap $DISK-part2 /dev/urandom swap,cipher=aes-xts-plain64:sha256,size=512 >> /etc/crypttab
 echo /dev/mapper/swap none swap defaults 0 0 >> /etc/fstab
-
+}
 
 echo "Adding system groups" && {
 addgroup --system lpadmin
@@ -53,15 +51,13 @@ mkdir /etc/zfs/zfs-list.cache
 touch /etc/zfs/zfs-list.cache/bpool
 touch /etc/zfs/zfs-list.cache/rpool
 ln -s /usr/lib/zfs-linux/zed.d/history_event-zfs-list-cacher.sh /etc/zfs/zed.d
-}
 echo "Running zed" && {
 timeout -s 15 -k 10 zed -F || true
-}
-echo "Fixing filesystem mounts" && {
+echo "Fixing filesystem mounts"
 sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/bpool
 sed -Ei "s|/mnt/?|/|" /etc/zfs/zfs-list.cache/rpool
-}
 echo "sed finished"
+}
 
 echo "Create user dataset" && {
 ROOT_DS=$(zfs list -o name | awk '/ROOT\/"$RDATASET"_/{print $1;exit}')
@@ -75,11 +71,13 @@ chown -R $USER:$USER /home/$USER
 usermod -a -G adm,cdrom,dip,lpadmin,lxd,plugdev,sambashare,sudo,gpio,i2c,input,spi $USER
 }
 
+echo "Disable log compression" && {
 for file in /etc/logrotate.d/* ; do
     if grep -Eq "(^|[^#y])compress" "$file" ; then
         sed -i -r "s/(^|[^#y])(compress)/\1#\2/" "$file"
     fi
 done
+}
 
 echo "Create inital system snapshot" && {
 zfs snapshot bpool/BOOT/"$RDATASET"_"$UUID"@bpool_install
