2c1601429d3140ded1a2ac20abe40c1b6b16e0b2
ArchivalLegion <55867818+ArchivalLegion@users.noreply.github.com>
2022-02-11 13:22:19 -0500

    Update 2ubuntu-minimal.sh

--- tmp/2ubuntu-minimal.sh
+++ tmp/2ubuntu-minimal.sh
@@ -7,7 +7,7 @@ echo "Update packages and configure system" && {
 	apt update
 	apt full-upgrade -yq
 	dpkg-reconfigure locales tzdata keyboard-configuration console-setup
-	apt install -yq nano dosfstools cryptsetup ubuntu-standard grub-efi-amd64 grub-efi-amd64-signed shim shim-signed linux-image-generic linux-image-lowlatency-hwe-20.04
+	apt install -yq nano dosfstools cryptsetup ubuntu-standard linux-image-generic linux-image-lowlatency-hwe-20.04
   }
 
 echo "Adding system groups" && {
@@ -27,9 +27,9 @@ echo "Create user" && {
 }
 
 echo "Install GRUB" && {
-	update-grub
-	grub-install --target=x86_64-efi --efi-directory=/boot/efi \
-	--bootloader-id=$BOOTID --recheck --removable -v
+	apt install grub-efi-amd64 grub-efi-amd64-signed shim shim-signed
+	update-initramfs -c -k all
+	grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=$BOOTID --recheck --removable
 	update-grub
 	}
 
